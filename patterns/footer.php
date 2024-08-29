cb5a02476840fb18856ed4dc546c9fccd00e09fe
John Russell <john@johnrussell.dev>
2024-08-29 11:51:26 -0600

    Updated the footer.php pattern to force some columns to stack on mobile and others to NOT

--- patterns/footer.php
+++ patterns/footer.php
@@ -8,8 +8,8 @@
  * Source: theme
  */
 ?>
-<!-- wp:group {"metadata":{"categories":["footer"],"patternName":"infinitum/footer","name":"Footer"},"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}},"heading":{"color":{"text":"var:preset|color|energy-green"}}},"spacing":{"padding":{"top":"var:preset|spacing|2"}}},"backgroundColor":"oval-green","textColor":"white","layout":{"inherit":true,"type":"constrained"}} -->
-<div class="wp-block-group site-footer alignfull has-white-color has-oval-green-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--2)">
+<!-- wp:group {"metadata":{"categories":["footer"],"patternName":"infinitum/footer","name":"Footer"},"align":"full","className":"site-footer","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}},"heading":{"color":{"text":"var:preset|color|energy-green"}}},"spacing":{"padding":{"top":"var:preset|spacing|2"}}},"backgroundColor":"oval-green","textColor":"white","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull site-footer has-white-color has-oval-green-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--2)">
 	<!-- wp:columns -->
 	<div class="wp-block-columns">
 		<!-- wp:column {"verticalAlignment":"center","width":"60%"} -->
@@ -18,17 +18,24 @@
 		</div>
 		<!-- /wp:column -->
 
-		<!-- wp:column {"width":"20%"} -->
-		<div class="wp-block-column" style="flex-basis:20%">
-			<!-- wp:paragraph {"fontSize":"90", "metadata":{"bindings":{"content":{"source":"cla/global-links-cla"}},"name":"Global Links (CLA)"}} --><p class="has-90-font-size"></p><!-- /wp:paragraph -->
-		</div>
-		<!-- /wp:column -->
+		<!-- wp:column {"width":"40%"} -->
+		<div class="wp-block-column" style="flex-basis:40%">
+			<!-- wp:columns {"isStackedOnMobile":false} -->
+			<div class="wp-block-columns is-not-stacked-on-mobile">
+				<!-- wp:column {"width":"50%"} -->
+				<div class="wp-block-column" style="flex-basis:50%">
+					<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"cla/global-links-cla"}},"name":"Global Links (CLA)"},"fontSize":"90"} --><p class="has-90-font-size"></p><!-- /wp:paragraph -->
+				</div>
+				<!-- /wp:column -->
 
-		<!-- wp:column {"width":"20%"} -->
-		<div class="wp-block-column" style="flex-basis:20%">
-			<!-- wp:paragraph {"fontSize":"90", "metadata":{"bindings":{"content":{"source":"cla/global-links-csu"}},"name":"Global Links (CSU)"}} --><p class="has-90-font-size"></p><!-- /wp:paragraph -->
-
-			<!-- wp:loginout {"fontSize":"90"} /-->	
+				<!-- wp:column {"width":"50%"} -->
+				<div class="wp-block-column" style="flex-basis:50%">
+					<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"cla/global-links-csu"}},"name":"Global Links (CSU)"},"fontSize":"90"} --><p class="has-90-font-size"></p><!-- /wp:paragraph -->
+					<!-- wp:loginout {"fontSize":"90"} /-->
+				</div>
+				<!-- /wp:column -->
+			</div>
+			<!-- /wp:columns -->
 		</div>
 		<!-- /wp:column -->
 	</div>
