bd005c091b9a86fcc498ba576aa6b31efbd11f98
John Russell <john@johnrussell.dev>
2024-09-18 12:41:37 -0600

    Removed blocks directory from being tracked

--- blocks/server-test/block.asset.php
+++ blocks/server-test/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-primitives'), 'version' => '5e4373935e434b2c467a');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-primitives'), 'version' => '2b969ce3c6acd3c7f5cc');
--- blocks/server-test/view.asset.php
+++ blocks/server-test/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '31d6cfe0d16ae931b73c');
+<?php return array('dependencies' => array(), 'version' => 'fcf2402010867328d46d');
